a3fd750ce0d0e99d60e7095f82359af457f1e78c
Gabe Fridkis <gabefridkis@gmail.com>
2023-02-22 17:52:41 -0500

    Initial Commit

---
+++ LibreWidget/LibreWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  LibreWidgetApp.swift
+//  LibreWidget
+//
+//  Created by Gabe Fridkis on 2/22/23.
+//
+
+import SwiftUI
+
+@main
+struct LibreWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
